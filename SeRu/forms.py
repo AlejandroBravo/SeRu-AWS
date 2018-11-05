f9ac98a7f4498796a2a5e02e3ee10f9760ac13d0
AlejandroBravo <alejandrobravotrabajo@gmail.com>
2018-11-05 11:50:27 +0100

    Extension del modelo de usuario finalizada

--- SeRu/forms.py
+++ SeRu/forms.py
@@ -15,6 +15,6 @@ class UserCreateForm(UserCreationForm):
         if not commit:
             raise NotImplementedError("No se puede crear un usuario y un perfil de usuario sin save() en la base de datos")
         user = super(UserCreateForm, self).save(commit=True)
-        user_profile = UserProfile(user=User, telefono_contacto=self.cleaned_data['telefono_contacto'],)
+        user_profile = User(telefono_contacto=self.cleaned_data['telefono_contacto'],)
         user_profile.save()
         return user, user_profile
